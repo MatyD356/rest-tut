29e6e386e90940b3e269925892cc59e3fb45537c
Mateusz <matyd356@gmail.com>
2021-03-18 22:19:21 +0100

    add cors and secure app port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('app listening on port 3000!');
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}!`);
 })
